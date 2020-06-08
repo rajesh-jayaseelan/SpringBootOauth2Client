a113e5d5a4f2be41d647c80daa4f89a6932a3813
Rajesh Jayaseelan <65347927+rajesh-jayaseelan@users.noreply.github.com>
2020-06-08 15:15:18 +0530

    added show emp page, instead for error page.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SpringBootOauth2Client
  SpringBootOauth2Client Run/Demo for Refs.
+ Ref url - https://dzone.com/articles/spring-boot-oauth2-getting-the-authorization-code
--- src/main/java/springbootsecurity/client/controllers/EmployeeController.java
+++ src/main/java/springbootsecurity/client/controllers/EmployeeController.java
@@ -1,9 +1,22 @@
 package springbootsecurity.client.controllers;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Base64;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 @Controller
 public class EmployeeController {
@@ -13,4 +26,12 @@ public class EmployeeController {
 		System.out.println("getEmployees uri called!");
         return new ModelAndView("getEmployees");
     }
+	
+	@RequestMapping(value = "/showEmployees", method = RequestMethod.GET)
+	public ModelAndView showEmployees(@RequestParam("code") String code) throws JsonProcessingException, IOException {
+		ResponseEntity<String> response = null;
+		System.out.println("Authorization Code------" + code);
+		return new ModelAndView("showEmployees");
+	}
+
 }
